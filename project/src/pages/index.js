a346eec37d4a2782e0da2994558e39fb4b48ebc6
Scott Milliorn <scottmilliorn@gmail.com>
2021-10-17 16:34:15 -0700

    refactor Application

--- project/src/pages/index.js
+++ project/src/pages/index.js
@@ -2,22 +2,21 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 import { siteMetadata } from '../../gatsby-config'
 
-class Application extends React.Component {
-  render() {
-    return (
-      <div className='application'>
-        <Helmet htmlAttributes={{
-          lang: 'en',
-        }}>
-          <meta charSet='utf-8' />
-          <title>Gatsby Recipes</title>
-          <link rel='canonical' href="https://samplegatsbyrecipepage.gatsbyjs.io/" />
-          <siteMetadata />
-        </Helmet>
-      </div>
-    )
-  }
+const Application = () => {
+  return (
+    <div className='application'>
+      <Helmet htmlAttributes={{
+        lang: 'en',
+      }}>
+        <meta charSet='utf-8' />
+        <title>Gatsby Recipes</title>
+        <link rel='canonical' href="https://samplegatsbyrecipepage.gatsbyjs.io/" />
+        <siteMetadata />
+      </Helmet>
+    </div>
+  )
 }
+
 export default function Home() {
   return (
     <>
