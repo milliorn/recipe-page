8d34056bfbe7b390df78eb0ac8bf94010e3d34cb
Scott Milliorn <scottmilliorn@gmail.com>
2021-10-17 01:54:54 -0700

    fixed react helmet

--- project/gatsby-config.js
+++ project/gatsby-config.js
@@ -2,56 +2,5 @@ const siteUrl = process.env.URL || `https://samplegatsbyrecipepage.gatsbyjs.io/`
 
 // In your gatsby-config.js
 module.exports = {
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        'gatsby-plugin-next-seo',
-        {
-            resolve: "gatsby-plugin-sitemap",
-            options: {
-                query: `
-        {
-          allSitePage {
-            nodes {
-              path
-            }
-          }
-          allWpContentNode(filter: {nodeType: {in: ["Post", "Page"]}}) {
-            nodes {
-              ... on WpPost {
-                uri
-                modifiedGmt
-              }
-              ... on WpPage {
-                uri
-                modifiedGmt
-              }
-            }
-          }
-        }
-      `,
-                resolveSiteUrl: () => siteUrl,
-                resolvePages: ({
-                    allSitePage: { nodes: allPages },
-                    allWpContentNode: { nodes: allWpNodes },
-                }) => {
-                    const wpNodeMap = allWpNodes.reduce((acc, node) => {
-                        const { uri } = node
-                        acc[uri] = node
-
-                        return acc
-                    }, {})
-
-                    return allPages.map(page => {
-                        return { ...page, ...wpNodeMap[page.path] }
-                    })
-                },
-                serialize: ({ path, modifiedGmt }) => {
-                    return {
-                        url: path,
-                        lastmod: modifiedGmt,
-                    }
-                },
-            },
-        },
-    ],
+  plugins: [`gatsby-plugin-react-helmet`]
 }
