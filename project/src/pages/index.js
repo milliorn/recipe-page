57ab5b36f5b8e8b0fb20cf748e6bc92620588cca
Scott Milliorn <scottmilliorn@gmail.com>
2021-10-17 02:09:09 -0700

    Adding Page Metadata

--- project/src/pages/index.js
+++ project/src/pages/index.js
@@ -1,8 +1,22 @@
 import React from "react"
-
+import { Helmet } from "react-helmet"
+class Application extends React.Component {
+  render() {
+    return (
+      <div className="application">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Gatsby Recipes</title>
+          <link rel="canonical" href="https://samplegatsbyrecipepage.gatsbyjs.io/" />
+        </Helmet>
+      </div>
+    )
+  }
+}
 export default function Home() {
   return (
     <>
+      <Application />
       <title>Test Page</title>
       <div>Hello world!</div>
     </>
