1f06656c32e650c9219b7eb574b277fe4a489f0d
Scott Milliorn <scottmilliorn@gmail.com>
2021-10-17 01:35:10 -0700

    lighthouse

--- project/gatsby-config.js
+++ project/gatsby-config.js
@@ -1,6 +1,57 @@
-/*
- * Configure your Gatsby site with this file.
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+const siteUrl = process.env.URL || `https://samplegatsbyrecipepage.gatsbyjs.io/`
 
-module.exports = {}
+// In your gatsby-config.js
+module.exports = {
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        'gatsby-plugin-next-seo',
+        {
+            resolve: "gatsby-plugin-sitemap",
+            options: {
+                query: `
+        {
+          allSitePage {
+            nodes {
+              path
+            }
+          }
+          allWpContentNode(filter: {nodeType: {in: ["Post", "Page"]}}) {
+            nodes {
+              ... on WpPost {
+                uri
+                modifiedGmt
+              }
+              ... on WpPage {
+                uri
+                modifiedGmt
+              }
+            }
+          }
+        }
+      `,
+                resolveSiteUrl: () => siteUrl,
+                resolvePages: ({
+                    allSitePage: { nodes: allPages },
+                    allWpContentNode: { nodes: allWpNodes },
+                }) => {
+                    const wpNodeMap = allWpNodes.reduce((acc, node) => {
+                        const { uri } = node
+                        acc[uri] = node
+
+                        return acc
+                    }, {})
+
+                    return allPages.map(page => {
+                        return { ...page, ...wpNodeMap[page.path] }
+                    })
+                },
+                serialize: ({ path, modifiedGmt }) => {
+                    return {
+                        url: path,
+                        lastmod: modifiedGmt,
+                    }
+                },
+            },
+        },
+    ],
+}
--- project/src/pages/index.js
+++ project/src/pages/index.js
@@ -1,5 +1,26 @@
 import React from "react"
+import { Helmet } from "react-helmet"
+import { GatsbySeo } from 'gatsby-plugin-next-seo';
 
+class Application extends React.Component {
+  render() {
+    return (
+      <div className="application">
+        <GatsbySeo
+          title='Simple Usage Example'
+          description='A short description goes here.'
+        />
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>My Title</title>
+          <link rel="canonical" href="https://samplegatsbyrecipepage.gatsbyjs.io/" />
+        </Helmet>
+      </div>
+    )
+  }
+}
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+  <><title>Test Page</title><div>Hello world!</div></>
+  )
 }
