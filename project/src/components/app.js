b81ed990158774075b980459e97f97bf16a1cadd
Scott Milliorn <scottmilliorn@gmail.com>
2021-10-17 16:49:44 -0700

    refactor

---
+++ project/src/components/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const app = () => {
+  return (
+    <div className='application'>
+      <Helmet htmlAttributes={{
+        lang: 'en',
+      }}>
+        <meta charSet='utf-8' />
+        <title>Gatsby Recipes</title>
+        <link rel='canonical' href="https://samplegatsbyrecipepage.gatsbyjs.io/" />
+        <siteMetadata />
+      </Helmet>
+    </div>
+  )
+}
+
+export default app
--- project/src/pages/index.js
+++ project/src/pages/index.js
@@ -1,27 +1,12 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
 import { siteMetadata } from '../../gatsby-config'
-
-const Application = () => {
-  return (
-    <div className='application'>
-      <Helmet htmlAttributes={{
-        lang: 'en',
-      }}>
-        <meta charSet='utf-8' />
-        <title>Gatsby Recipes</title>
-        <link rel='canonical' href="https://samplegatsbyrecipepage.gatsbyjs.io/" />
-        <siteMetadata />
-      </Helmet>
-    </div>
-  )
-}
+import app from '../components/app'
 
 export default function Home() {
   return (
     <>
-      <Application />
-      <div>Hello world!</div>
+      <app/>
+      <div>Hello {siteMetadata.title}!</div>
     </>
   )
 }
