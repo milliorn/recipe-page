eda0f10ef83b0d6f1b785496b855a69becfd10f4
Scott Milliorn <scottmilliorn@gmail.com>
2021-10-26 21:10:42 -0700

    styles

--- project/src/components/layout.jsx
+++ project/src/components/layout.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from './navbar'
 import Footer from './footer'
+import 'normalize.css'
+import '../assets/css/main.css'
 
 const Layout = ({ children }) => {
   return (
