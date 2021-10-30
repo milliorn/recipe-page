58724257cd7b9dcd2a8aca0e7870dc77a92af137
Scott Milliorn <scottmilliorn@gmail.com>
2021-10-29 18:00:34 -0700

    navbar setup

--- project/src/components/navbar.jsx
+++ project/src/components/navbar.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
 import { Link } from 'gatsby'
-import { ImHome } from 'react-icons/im'
+import React from 'react'
+import { FiAlignJustify } from 'react-icons/fi'
+import logo from '../assets/images/logo.svg'
+
 const Navbar = () => {
   return (
-    <nav>
-      <ImHome />
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/tags">Tags</Link>
-        </li>
-        <li>
-          <Link to="/recipes">Recipes</Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="simply recipes" />
+          </Link>
+          <button className="nav-btn">
+            <FiAlignJustify />
+          </button>
+        </div>
+        <div className="nav-links show-links">
+          <Link to="/" className="nav-link" activeClassName="active-link">
+            home
+          </Link>
+          <Link
+            to="/recipes"
+            className="nav-link"
+            activeClassName="active-link"
+          >
+            recipes
+          </Link>
+          <Link to="/tags" className="nav-link" activeClassName="active-link">
+            tags
+          </Link>
+          <Link to="/about" className="nav-link" activeClassName="active-link">
+            about
+          </Link>
+          <div className="nav-link contact-link">
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </div>
+        </div>
+      </div>
     </nav>
   )
 }
