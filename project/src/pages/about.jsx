5e94605a7bdce1bef2ab5f2c22459b7d5cd6642e
Scott Milliorn <scottmilliorn@gmail.com>
2021-11-10 01:22:04 -0800

    revert

--- project/src/pages/about.jsx
+++ project/src/pages/about.jsx
@@ -4,22 +4,38 @@ import { StaticImage } from 'gatsby-plugin-image'
 import { Link } from 'gatsby'
 
 const About = () => {
-	return (
+  return (
     <Layout>
-      <main className='page'>
-        <section className='about-page'>
+      <main className="page">
+        <section className="about-page">
           <article>
-            <h2></h2>
-            <p></p>
-            <p></p>
-            <Link to='/contact' className='btn'>
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit!</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem
+              tenter dicta ut voluptas aliquam placeat. Odio, minus rerum?
+              Possimus quaerat et ab ipsum accusamus, id corporis perspiciatis
+              omnis. Soluta, ab?
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Aspernatur tempore laudantium ad perspiciatis dolorem est omnis
+              consectetur tenetur dolorum, nisi cupiditate quidem similique
+              porro laboriosam aut nam vel veritatis veniam!
+            </p>
+            <Link to="/contact" className="btn">
               contact
             </Link>
           </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="about"
+            className="about-img"
+            placeholder="blurred"
+          ></StaticImage>
         </section>
       </main>
-		</Layout>
-	)
+    </Layout>
+  )
 }
 
 export default About
