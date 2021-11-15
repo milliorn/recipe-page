363e1faf85c1c3a230f7de18d8d95b60ff27e3ea
Scott Milliorn <scottmilliorn@gmail.com>
2021-11-14 21:53:49 -0800

    Hero

--- project/src/pages/index.jsx
+++ project/src/pages/index.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 export default function Home() {
   return (
     <Layout>
-      <div className="home-heading">
-        <h1 style={{ color: 'green', textTransform: 'capitalize' }}>
-          Hello Home Page!
-        </h1>
-      </div>
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="eggs"
+            className='"hero-img'
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          ></StaticImage>
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>React Recipes</h1>
+              <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            </div>
+          </div>
+        </header>
+      </main>
     </Layout>
   )
 }
