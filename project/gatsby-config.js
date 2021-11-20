e71a6f7d6206dd2f6d4c332b79be480cc2c67fe6
Scott Milliorn <scottmilliorn@gmail.com>
2021-11-19 21:24:37 -0800

    siteMetadata

--- project/gatsby-config.js
+++ project/gatsby-config.js
@@ -1,21 +1,24 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Recipes`,
-    description: `Blazing fast modern site generator for React`,
-    siteUrl: `https://samplegatsbyrecipepage.gatsbyjs.io`
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images`
-      }
-    }
-  ]
+	siteMetadata: {
+		title: `Gatsby Recipes`,
+		description: `Blazing fast modern site generator for React`,
+		siteUrl: `https://samplegatsbyrecipepage.gatsbyjs.io`,
+		author: 'Scott Milliorn',
+		github: 'https://github.com/milliorn/Sample-Gatsby-Recipe-Page',
+		creationDate: new Date(2021, 10, 16)
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-styled-components`,
+		`gatsby-plugin-image`,
+		`gatsby-plugin-sharp`,
+		`gatsby-transformer-sharp`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/assets/images`
+			}
+		}
+	]
 }
