327b28c17fe49827bf1ce63d4420f23ec0f6da81
Scott Milliorn <scottmilliorn@gmail.com>
2021-11-28 22:47:12 -0800

    contentful, ENV Variables

--- project/gatsby-config.js
+++ project/gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
 module.exports = {
   siteMetadata: {
     author: 'Scott Milliorn',
@@ -18,6 +22,14 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/assets/images`
+      }
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `6p9utpb04y6n`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_API_KEY
       }
     }
   ]
