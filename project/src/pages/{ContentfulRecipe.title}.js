0bb09d2239b4e68f0a253cab473acf3b84fec929
Scott Milliorn <scottmilliorn@gmail.com>
2021-12-10 18:40:51 -0800

    getSingleRecipe

--- project/src/pages/{ContentfulRecipe.title}.js
+++ project/src/pages/{ContentfulRecipe.title}.js
@@ -1,3 +1,4 @@
+import { graphql } from 'gatsby'
 import React from 'react'
 
 const RecipeTemplate = props => {
@@ -7,5 +8,28 @@ const RecipeTemplate = props => {
     </div>
   )
 }
+
+export const query = graphql`
+  query getSingleRecipe($title: String) {
+    contentfulRecipe(title: { eq: $title }) {
+      title
+      cookTime
+      content {
+        ingredients
+        instructions
+        tags
+        tools
+      }
+      description {
+        description
+      }
+      prepTime
+      servings
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+      }
+    }
+  }
+`
 
 export default RecipeTemplate
