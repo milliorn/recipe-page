6dc85cde89c8d2e25072a2f22e68e598b6dd1239
Scott Milliorn <scottmilliorn@gmail.com>
2021-12-11 22:45:15 -0800

    data.contentfulRecipe

--- project/src/pages/{ContentfulRecipe.title}.js
+++ project/src/pages/{ContentfulRecipe.title}.js
@@ -1,11 +1,27 @@
-import { graphql } from 'gatsby'
 import React from 'react'
+import { BsClockHistory, BsClock, BsPeople } from 'react-icons'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { graphql } from 'gatsby'
 
-const RecipeTemplate = props => {
+const RecipeTemplate = ({ data }) => {
+  const {
+    content,
+    cookTime,
+    description: { description },
+    image,
+    prepTime,
+    servings,
+    title
+  } = data.contentfulRecipe
+
+  const pathToImage = getImage(image)
+  const { tags, instructions, ingredients, tools } = content
   return (
-    <div>
-      <h4>{props.params.title}</h4>
-    </div>
+    <main className="page">
+      <div className="recipe-page">
+        <h2>{title}</h2>
+      </div>
+    </main>
   )
 }
 
