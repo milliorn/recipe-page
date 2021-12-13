e38fc802123f97fb27678874dbcd5098085fe5bd
Scott Milliorn <scottmilliorn@gmail.com>
2021-12-12 18:52:28 -0800

    RecipeTemplate

--- project/src/pages/{ContentfulRecipe.title}.js
+++ project/src/pages/{ContentfulRecipe.title}.js
@@ -1,27 +1,112 @@
-import React from 'react'
-import { BsClockHistory, BsClock, BsPeople } from 'react-icons'
+import { BsClockHistory, BsClock, BsPeople } from 'react-icons/bs'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
+import Layout from '../components/Layout'
+import React from 'react'
+import slugify from 'slugify'
 
 const RecipeTemplate = ({ data }) => {
   const {
-    content,
+    title,
     cookTime,
-    description: { description },
-    image,
+    content,
     prepTime,
     servings,
-    title
+    description: { description },
+    image
   } = data.contentfulRecipe
-
   const pathToImage = getImage(image)
   const { tags, instructions, ingredients, tools } = content
   return (
-    <main className="page">
-      <div className="recipe-page">
-        <h2>{title}</h2>
-      </div>
-    </main>
+    <Layout>
+      <main className="page">
+        <div className="recipe-page">
+          <section className="recipe-hero">
+            <GatsbyImage
+              image={pathToImage}
+              alt={title}
+              className="about-img"
+            />
+
+            <article className="recipe-info">
+              <h2>{title}</h2>
+              <p>{description}</p>
+
+              <div className="recipe-icons">
+                <article>
+                  <BsClock />
+                  <h5>prep time</h5>
+                  <p>{prepTime} min.</p>
+                </article>
+                <article>
+                  <BsClockHistory />
+                  <h5>cook time</h5>
+                  <p>{cookTime} min.</p>
+                </article>
+                <article>
+                  <BsPeople />
+                  <h5>serving</h5>
+                  <p>{servings} </p>
+                </article>
+              </div>
+
+              <p className="recipe-tags">
+                Tags :
+                {tags.map((tag, index) => {
+                  const slug = slugify(tag, { lower: true })
+
+                  return (
+                    <Link to={`/tags/${slug}`} key={index}>
+                      {tag}
+                    </Link>
+                  )
+                })}
+              </p>
+            </article>
+          </section>
+
+          <section className="recipe-content">
+            <article>
+              <h4>instructions</h4>
+              {instructions.map((item, index) => {
+                return (
+                  <div key={index} className="single-instruction">
+                    <header>
+                      <p>step {index + 1}</p>
+                      <div></div>
+                    </header>
+                    <p>{item}</p>
+                  </div>
+                )
+              })}
+            </article>
+
+            <article className="second-column">
+              <div>
+                <h4>ingredients</h4>
+                {ingredients.map((item, index) => {
+                  return (
+                    <p key={index} className="single-ingredient">
+                      {item}
+                    </p>
+                  )
+                })}
+              </div>
+              <div>
+                <h4>tools</h4>
+                {tools.map((item, index) => {
+                  return (
+                    <p key={index} className="single-tool">
+                      {item}
+                    </p>
+                  )
+                })}
+              </div>
+            </article>
+          </section>
+        </div>
+      </main>
+    </Layout>
   )
 }
 
