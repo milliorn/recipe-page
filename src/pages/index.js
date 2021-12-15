fcdebfcc0f2426c639aa266d3f89ac6029e357f1
Restyled.io <commits@restyled.io>
2021-12-15 22:32:54 +0000

    Restyled by prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
-import {StaticImage} from 'gatsby-plugin-image'
-import React, {useEffect, useState} from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
+import React, { useEffect, useState } from 'react'
 
 import AllRecipes from '../components/AllRecipes'
 import Layout from '../components/Layout'
@@ -13,11 +13,14 @@ https://www.pluralsight.com/guides/process-an-api-response-in-react
 export default function Home() {
   const [comments, setComments] = useState([])
 
-  useEffect(() => {fetchComments()}, [])
+  useEffect(() => {
+    fetchComments()
+  }, [])
 
   const fetchComments = async () => {
-    const response =
-        await Axios('https://my-bao-server.herokuapp.com/api/breadpuns')
+    const response = await Axios(
+      'https://my-bao-server.herokuapp.com/api/breadpuns'
+    )
     setComments(response.data)
   }
 
@@ -27,10 +30,10 @@ export default function Home() {
       <main className="page">
         <header className="hero">
           <StaticImage
-  src = "../assets/images/main.jpeg"
-  alt = "eggs"
-  className = "hero-img"
-  placeholder = "tracedSVG"
+            src="../assets/images/main.jpeg"
+            alt="eggs"
+            className="hero-img"
+            placeholder="tracedSVG"
             layout="fullWidth"
           />
           <div className="hero-container">
