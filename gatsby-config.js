7c67df0d3494ca6e4e4fdbfc129b8c97cae81df6
Scott Milliorn <scottmilliorn@gmail.com>
2022-04-12 23:43:56 -0700

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 /**
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
 })
 
 module.exports = {
@@ -10,36 +10,24 @@ module.exports = {
   siteMetadata: {
     author: 'Scott Milliorn',
     creationDate: new Date(2021, 10, 16),
-    description: `Blazing fast modern recipe site in GatsbyJS and React`,
-    github: 'https://github.com/milliorn/Sample-Gatsby-Recipe-Page',
-    siteUrl: `https://samplegatsbyrecipepage.gatsbyjs.io`,
-    title: `Gatsby Recipes`
+    description: `Blazing fast modern recipe site in GatsbyJS`,
+    title: "Simply Recipes",
+    github: 'https://github.com/milliorn/Recipe-Page',
+    siteUrl: `https://gatsbyrecipes.gatsbyjs.io/`,
+
+
   },
   plugins: [
-    `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Recipes`,
-        short_name: `Gatsby Recipes`,
-        start_url: `/`,
-        background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
-        display: `standalone`,
-        icon: `static/favicon-3.png`,
-        crossOrigin: `use-credentials` // `use-credentials` or `anonymous`
-      }
-    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/assets/images`
-      }
+        path: `${__dirname}/src/assets/images`,
+      },
     },
     {
       resolve: `gatsby-source-contentful`,
@@ -47,7 +35,7 @@ module.exports = {
         spaceId: `6p9utpb04y6n`,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_API_KEY
-      }
+      },
     },
     {
       resolve: `gatsby-plugin-webfonts`,
@@ -55,17 +43,17 @@ module.exports = {
         fonts: {
           google: [
             {
-              family: 'Montserrat',
-              variants: ['400']
+              family: "Montserrat",
+              variants: [ "400" ],
             },
             {
-              family: 'Inconsolata',
-              variants: ['400', '500', '600', '700']
-            }
-          ]
-        }
-      }
+              family: "Inconsolata",
+              variants: [ "400", "500", "600", "700" ],
+            },
+          ],
+        },
+      },
     },
-    `gatsby-plugin-react-helmet`
-  ]
+    `gatsby-plugin-react-helmet`,
+  ],
 }
