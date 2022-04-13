d50b3b09b109ba89b962ce62ba9a59e60bf5628c
Restyled.io <commits@restyled.io>
2022-04-13 06:44:40 +0000

    Restyled by prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,58 +1,57 @@
 /**
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-require("dotenv").config({
-  path : `.env.${process.env.NODE_ENV}`,
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
 })
 
 module.exports = {
   /* Your site config here */
-  siteMetadata : {
-    author : 'Scott Milliorn',
-    creationDate : new Date(2021, 10, 16),
-    description : `Blazing fast modern recipe site in GatsbyJS`,
-    title : "Simply Recipes",
-    github : 'https://github.com/milliorn/Recipe-Page',
-    siteUrl : `https://gatsbyrecipes.gatsbyjs.io/`,
-
+  siteMetadata: {
+    author: 'Scott Milliorn',
+    creationDate: new Date(2021, 10, 16),
+    description: `Blazing fast modern recipe site in GatsbyJS`,
+    title: 'Simply Recipes',
+    github: 'https://github.com/milliorn/Recipe-Page',
+    siteUrl: `https://gatsbyrecipes.gatsbyjs.io/`
   },
-  plugins : [
+  plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     {
-      resolve : `gatsby-source-filesystem`,
-      options : {
-        name : `images`,
-        path : `${__dirname}/src/assets/images`,
-      },
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`
+      }
     },
     {
-      resolve : `gatsby-source-contentful`,
-      options : {
-        spaceId : `6p9utpb04y6n`,
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `6p9utpb04y6n`,
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken : process.env.CONTENTFUL_API_KEY
-      },
+        accessToken: process.env.CONTENTFUL_API_KEY
+      }
     },
     {
-      resolve : `gatsby-plugin-webfonts`,
-      options : {
-        fonts : {
-          google : [
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
             {
-              family : "Montserrat",
-              variants : [ "400" ],
+              family: 'Montserrat',
+              variants: ['400']
             },
             {
-              family : "Inconsolata",
-              variants : [ "400", "500", "600", "700" ],
-            },
-          ],
-        },
-      },
+              family: 'Inconsolata',
+              variants: ['400', '500', '600', '700']
+            }
+          ]
+        }
+      }
     },
-    `gatsby-plugin-react-helmet`,
-  ],
+    `gatsby-plugin-react-helmet`
+  ]
 }
