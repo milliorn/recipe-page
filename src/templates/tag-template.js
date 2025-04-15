f9b0bd31ae1c243d71b3f3087c8c0923f4efd48b
milliorn <scottmilliorn@gmail.com>
2025-04-15 16:37:14 -0700

    update tag templates with fallback

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -2,10 +2,28 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import React from 'react'
 import RecipesList from '../components/RecipesList'
+// Uncomment the next line once you decide to use the SEO component
 // import SiteSeo from '../components/SiteSeo'
-/* Setup boilerplate code for tags to display on page */
+
+/*
+  TagTemplate: A page template to display recipes filtered by a tag.
+  It uses pageContext to get the tag name and queries Contentful for recipes that match the tag.
+*/
 const TagTemplate = ({ data, pageContext }) => {
   const recipes = data.allContentfulRecipe.nodes
+
+  // Add a fallback if no recipes are found for the tag
+  if (!recipes.length) {
+    return (
+      <Layout>
+        <main className="page">
+          <h2>{pageContext.tag}</h2>
+          <p>No recipes found for this tag.</p>
+        </main>
+      </Layout>
+    )
+  }
+
   return (
     <Layout>
       <main className="page">
@@ -14,7 +32,6 @@ const TagTemplate = ({ data, pageContext }) => {
           <RecipesList recipes={recipes} />
         </div>
       </main>
-      {/* <Head pageContext={pageContext} /> */}
     </Layout>
   )
 }
@@ -39,7 +56,3 @@ export const query = graphql`
 `
 
 export default TagTemplate
-
-// export const Head = ({ pageContext }) => {
-//   return <SiteSeo title={pageContext.tag} />
-// }
