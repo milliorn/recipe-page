cf0c9b751991c009cc1123ee097f62a5e26f7df9
milliorn <scottmilliorn@gmail.com>
2025-04-15 01:30:04 -0700

    Corrected Fallback Logic

--- src/components/SiteSeo.js
+++ src/components/SiteSeo.js
@@ -6,8 +6,10 @@ const SiteSeo = ({ title, description, children }) => {
   const { title: defaultTitle, description: defaultDescription } =
     useSiteMetadata()
 
+  const seoTitle = title ? `${title} | Simply Recipes` : defaultTitle;
+
   const seo = {
-    title: `${title} | Simply Recipes` || defaultTitle,
+    title: seoTitle,
     description: description || defaultDescription
   }
 
